61f247ae747967fe6e540981a5faba316834640d
Raimonds Simanovskis <raimonds.simanovskis@gmail.com>
2010-11-09 19:53:19 +0200

    testing memory server JSONP API

--- calculator_spec.js
+++ calculator_spec.js
@@ -93,4 +93,53 @@ describe("Calculator", function() {
       expect(calculator.accumulator()).toEqual(3);
     });
   });
+
+  describe("memory", function() {
+    var memory,
+        callback = function(data) {console.log(data); memory = typeof data === "string" ? parseInt(data,10) : data;};
+
+    beforeEach(function() {
+      calculator = new Calculator;
+      $.get("http://localhost:8080/memory/clear");
+    });
+
+    it("should initially be empty", function() {
+      // expect(calculator.isMemoryEmpty).toBeTruthy();
+      $.get("http://localhost:8080/memory", callback, "jsonp");
+      waits(100);
+      runs(function() {
+        expect(memory).toBeNull();
+      });
+    });
+
+    it("should add accumulator", function() {
+      // calculator
+      //   .accumulator(42)
+      //   .memoryAdd()
+      //   .accumulator(0)
+      //   .memoryRecall();
+      // expect(calculator.accumulator()).toEqual(42);
+      $.get("http://localhost:8080/memory/add/42", callback, "jsonp");
+      waits(100);
+      runs(function() {
+        expect(memory).toEqual(42);
+      });
+    });
+
+    it("should subtract accumulator", function() {
+      // calculator
+      //   .accumulator(42)
+      //   .memorySubtract()
+      //   .accumulator(0)
+      //   .memoryRecall();
+      // expect(calculator.accumulator()).toEqual(-42);
+      $.get("http://localhost:8080/memory/subtract/42", callback, "jsonp");
+      waits(100);
+      runs(function() {
+        expect(memory).toEqual(-42);
+      });
+    });
+
+  });
+
 });
