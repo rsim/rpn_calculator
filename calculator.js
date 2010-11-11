37dcce17a27255831d98a7fe2793fe72535edfae
Raimonds Simanovskis <raimonds.simanovskis@gmail.com>
2010-11-11 11:33:16 +0200

    added Backbone.js and added change event triggering to Calculator

--- calculator.js
+++ calculator.js
@@ -48,33 +48,44 @@ Stack.prototype = {
 
 var ServerMemory = function(url) {
   this.url = url;
-  this._lastValue = undefined;
+  this._lastValue = null;
 };
 
 ServerMemory.prototype = {
-  clear: function() {
-    $.get(this.url + "/memory/clear", this._setLastValue, "jsonp");
+  clear: function(callback) {
+    var self = this;
+    $.get(this.url + "/memory/clear", function(data) {
+      self._setLastValue(data, callback);
+    }, "jsonp");
     this._lastValue = null;
   },
 
-  add: function(value) {
-    $.get(this.url + "/memory/add/"+value, this._setLastValue, "jsonp");
+  add: function(value, callback) {
+    var self = this;
+    $.get(this.url + "/memory/add/"+value, function(data) {
+      self._setLastValue(data, callback);
+    }, "jsonp");
   },
 
-  subtract: function(value) {
-    $.get(this.url + "/memory/subtract/"+value, this._setLastValue, "jsonp");
+  subtract: function(value, callback) {
+    var self = this;
+    $.get(this.url + "/memory/subtract/"+value, function(data) {
+      self._setLastValue(data, callback);
+    }, "jsonp");
   },
 
   recall: function(callback) {
     var self = this;
     $.get(this.url + "/memory", function(data) {
-      self._setLastValue(data);
-      callback(data);
+      self._setLastValue(data, callback);
     }, "jsonp");
   },
 
-  _setLastValue: function(data) {
+  _setLastValue: function(data, callback) {
     this._lastValue = data;
+    if (typeof callback === "function") {
+      callback(data);
+    }
   },
 
   isEmpty: function() {
@@ -87,24 +98,27 @@ var Calculator = function() {
   this.memory = new ServerMemory("http://localhost:8080");
 };
 
-Calculator.prototype = {
+_.extend(Calculator.prototype, Backbone.Events, {
   accumulator: function(value) {
     if (typeof value === "undefined") {
       return this.stack.peek();
     } else {
       this.stack.replaceTop(value);
+      this.trigger("change:stack");
       return this;
     }
   },
 
   enter: function() {
     this.stack.push(this.stack.peek());
+    this.trigger("change:stack");
     return this;
   },
 
   drop: function() {
     this.stack.pop();
     this.stack.initialize();
+    this.trigger("change:stack");
     return this;
   },
 
@@ -140,33 +154,46 @@ Calculator.prototype = {
     } else {
       this.stack.push(implementation(lhs,rhs));
     }
+    this.trigger("change:stack");
     return this;
   },
 
   negate: function() {
     this.stack.replaceTop(-this.stack.peek());
+    this.trigger("change:stack");
     return this;
   },
 
   memoryClear: function() {
     this.memory.clear();
+    this.trigger("change:memory");
     return this;
   },
 
   memoryAdd: function() {
-    this.memory.add(this.stack.peek());
+    var self = this;
+    this.memory.add(this.stack.peek(), function(value) {
+      self.trigger("change:memory");
+    });
     return this;
   },
 
   memorySubtract: function() {
-    this.memory.subtract(this.stack.peek());
+    var self = this;
+    this.memory.subtract(this.stack.peek(), function(value) {
+      self.trigger("change:memory");
+    });
     return this;
   },
 
   memoryRecall: function() {
     var self = this;
     this.memory.recall(function(value) {
-      self.stack.replaceTop(value);
+      self.trigger("change:memory");
+      if (value !== null) {
+        self.stack.replaceTop(value);
+        self.trigger("change:stack");
+      }
     });
     return this;
   },
@@ -182,17 +209,21 @@ Calculator.prototype = {
   swap: function() {
     var rhs = this.stack.pop(),
         lhs = this.stack.pop();
-    this.stack.push(rhs).push(lhs);
+    this.stack.push(rhs);
+    if (typeof lhs !== "undefined") this.stack.push(lhs);
+    this.trigger("change:stack");
     return this;
   },
 
   rollDown: function() {
     this.stack.rollDown();
+    this.trigger("change:stack");
     return this;
   },
 
   rollUp: function() {
     this.stack.rollUp();
+    this.trigger("change:stack");
     return this;
   }
-};
+});
--- calculator_spec.js
+++ calculator_spec.js
@@ -182,4 +182,111 @@ describe("Calculator", function() {
     });
 
   });
+
+  describe("stack change event", function() {
+    var triggered,
+        triggerCallback = function() {triggered = true;};
+    beforeEach(function() {
+      calculator = new Calculator;
+      calculator.accumulator(3).enter().accumulator(4);
+      triggered = false;
+      calculator.bind("change:stack", triggerCallback);
+    });
+
+    it("should not be triggered initially", function() {
+      expect(triggered).toBeFalsy();
+    });
+
+    it("should be triggered on setting accumulator", function() {
+      calculator.accumulator(5);
+      expect(triggered).toBeTruthy();
+    });
+
+    it("should be triggered on enter", function() {
+      calculator.enter();
+      expect(triggered).toBeTruthy();
+    });
+
+    it("should be triggered on drop", function() {
+      calculator.drop();
+      expect(triggered).toBeTruthy();
+    });
+
+    it("should be triggered on binary operation", function() {
+      calculator.add();
+      expect(triggered).toBeTruthy();
+    });
+
+    it("should be triggered on negate", function() {
+      calculator.negate();
+      expect(triggered).toBeTruthy();
+    });
+
+    it("should be triggered on memory recall when memory has stored value", function() {
+      spyOn(calculator.memory, "recall").andCallFake(function(callback) {callback(1);});
+      calculator.memoryRecall();
+      expect(triggered).toBeTruthy();
+    });
+
+    it("should not be triggered on memory recall when memory has not stored value", function() {
+      spyOn(calculator.memory, "recall").andCallFake(function(callback) {callback(null);});
+      calculator.memoryRecall();
+      expect(triggered).toBeFalsy();
+    });
+
+    it("should be triggered on swap", function() {
+      calculator.swap();
+      expect(triggered).toBeTruthy();
+    });
+
+    it("should be triggered on roll down", function() {
+      calculator.rollDown();
+      expect(triggered).toBeTruthy();
+    });
+
+    it("should be triggered on roll up", function() {
+      calculator.rollUp();
+      expect(triggered).toBeTruthy();
+    });
+  });
+
+  describe("memory change event", function() {
+    var triggered,
+        triggerCallback = function() {triggered = true;};
+    beforeEach(function() {
+      calculator = new Calculator;
+      calculator.accumulator(1);
+      triggered = false;
+      calculator.bind("change:memory", triggerCallback);
+    });
+
+    it("should not be triggered initially", function() {
+      expect(triggered).toBeFalsy();
+    });
+
+    it("should be triggered on memory clear", function() {
+      spyOn(calculator.memory, "clear").andCallFake(function() {;});
+      calculator.memoryClear();
+      expect(triggered).toBeTruthy();
+    });
+
+    it("should be triggered on memory add", function() {
+      spyOn(calculator.memory, "add").andCallFake(function(value, callback) {callback(value);});
+      calculator.memoryAdd();
+      expect(triggered).toBeTruthy();
+    });
+
+    it("should be triggered on memory subtract", function() {
+      spyOn(calculator.memory, "subtract").andCallFake(function(value, callback) {callback(-value);});
+      calculator.memorySubtract();
+      expect(triggered).toBeTruthy();
+    });
+
+    it("should be triggered on memory recall", function() {
+      spyOn(calculator.memory, "recall").andCallFake(function(callback) {callback(1);});
+      calculator.memoryRecall();
+      expect(triggered).toBeTruthy();
+    });
+  });
+
 });
